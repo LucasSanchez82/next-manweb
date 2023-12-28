3f9a279494cde7808caf61ce4bd3b84c9a020d3d
Lucas <08.lucas.sanchez@gmail.com>
2023-12-28 01:35:47 +0100

    test build

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,29 +8,29 @@ async function main() {
   // Votre code Prisma pour insérer des données si nécessaire
 
   // Exécutez le script SQL
-  await prisma.$executeRaw` 
-    -- Placez ici votre script SQL avec les déclencheurs
-    DELIMITER //
-    CREATE TRIGGER before_manga_insert
-    AFTER INSERT ON Manga
-    FOR EACH ROW
-    BEGIN
-        INSERT INTO HistoriqueManga (mangaId, title, linkManga, linkImage, chapter)
-        VALUES (NEW.id, NEW.title, NEW.linkManga, NEW.linkImage, NEW.chapter);
-    END;
-    //
+  // await prisma.$executeRaw` 
+  //   -- Placez ici votre script SQL avec les déclencheurs
+  //   DELIMITER //
+  //   CREATE TRIGGER before_manga_insert
+  //   AFTER INSERT ON Manga
+  //   FOR EACH ROW
+  //   BEGIN
+  //       INSERT INTO HistoriqueManga (mangaId, title, linkManga, linkImage, chapter)
+  //       VALUES (NEW.id, NEW.title, NEW.linkManga, NEW.linkImage, NEW.chapter);
+  //   END;
+  //   //
 
-    CREATE TRIGGER before_manga_update
-    AFTER UPDATE ON Manga
-    FOR EACH ROW
-    BEGIN
-            INSERT INTO HistoriqueManga (mangaId, title, linkManga, linkImage, chapter)
-            VALUES (NEW.id, NEW.title, NEW.linkManga, NEW.linkImage, NEW.chapter);
-    END;
-    //
-    DELIMITER ;
+  //   CREATE TRIGGER before_manga_update
+  //   AFTER UPDATE ON Manga
+  //   FOR EACH ROW
+  //   BEGIN
+  //           INSERT INTO HistoriqueManga (mangaId, title, linkManga, linkImage, chapter)
+  //           VALUES (NEW.id, NEW.title, NEW.linkManga, NEW.linkImage, NEW.chapter);
+  //   END;
+  //   //
+  //   DELIMITER ;
 
-  `;
+  // `;
 
   // Fermez la connexion Prisma
   await prisma.$disconnect();
