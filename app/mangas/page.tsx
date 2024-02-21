e8c620e4cb4895a98006f621b42f4a126f0fa654
Lucas <jiraya1008@gmail.com>
2024-02-21 23:17:29 +0100

    Refactor session handling and add sessionProvider

--- app/mangas/page.tsx
+++ app/mangas/page.tsx
@@ -3,57 +3,56 @@ import { getSafeSessionServer, prisma } from "@/lib/utils";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import MangaContainer from "../../components/mangas/mangaContainer.component";
+import { sessionProvider } from "@/lib/utilsSession";
 
 const page = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) => {
-  const session = await getSafeSessionServer();
-  if (!session) {
-    redirect("/");
-  }
-  const cookieSore = cookies();
-  const searchedValue = String(cookieSore.get("searchedTitle")?.value || "");
+  return sessionProvider(async (session) => {
+    const cookieSore = cookies();
+    const searchedValue = String(cookieSore.get("searchedTitle")?.value || "");
 
-  const nbAffiche = Number(process.env.MANGAS_NB_AFFICHE || 5);
-  const currPage = getPage();
-  const nbMangas = await prisma.manga.count({
-    where: {
-      userId: session.user.userId,
-      title: {
-        contains: searchedValue,
+    const nbAffiche = Number(process.env.MANGAS_NB_AFFICHE || 5);
+    const currPage = getPage();
+    const nbMangas = await prisma.manga.count({
+      where: {
+        userId: session.user.userId,
+        title: {
+          contains: searchedValue,
+        },
+        isDeleted: false,
       },
-      isDeleted: false,
-    },
-  });
+    });
 
-  const nbTotalPages = Math.round(nbMangas / nbAffiche);
-  const mangas = await prisma.manga.findMany({
-    where: {
-      userId: session.user.userId,
-      title: {
-        contains: searchedValue,
+    const nbTotalPages = Math.round(nbMangas / nbAffiche);
+    const mangas = await prisma.manga.findMany({
+      where: {
+        userId: session.user.userId,
+        title: {
+          contains: searchedValue,
+        },
+        isDeleted: false,
       },
-      isDeleted: false,
-    },
-    orderBy: {
-      id: "desc",
-    },
-    take: nbAffiche,
-    skip: nbAffiche * (currPage - 1),
-  });
+      orderBy: {
+        id: "desc",
+      },
+      take: nbAffiche,
+      skip: nbAffiche * (currPage - 1),
+    });
 
-  return (
-    <main className="flex flex-col justify-center items-center text-center h-full">
-      <MangaContainer
-        nbTotalPages={nbTotalPages}
-        nbAffiche={nbAffiche}
-        mangas={mangas}
-        page={currPage}
-      />
-    </main>
-  );
+    return (
+      <main className="flex flex-col justify-center items-center text-center h-full">
+        <MangaContainer
+          nbTotalPages={nbTotalPages}
+          nbAffiche={nbAffiche}
+          mangas={mangas}
+          page={currPage}
+        />
+      </main>
+    );
+  });
 };
 
 export default page;
--- lib/utilsSession.ts
+++ lib/utilsSession.ts
@@ -1,9 +1,10 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { Session, getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export const sessionProvider = async (fn: (sessionValue: Session) => any) => {
+export const sessionProvider = async (fn: (sessionValue: Session) => any, elseFn?: () => any) => {
     const session = await getServerSession(authOptions);
     if(session) {
         return fn(session);
-    }else throw new Error("No session found");
+    }else elseFn ? elseFn() : redirect('/');
 }
